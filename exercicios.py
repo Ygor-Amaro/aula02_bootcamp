b885c801460718fb95a8fbbd5777af815b5cdade
Ygor Amaro <ykba09@gmail.com>
2025-04-10 22:14:48 -0300

    exercicio 08

--- exercicios.py
+++ exercicios.py
@@ -3,58 +3,58 @@
 ## Inteiros (int)
 ## 01 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
-ex1_num1 = int(input("Digite o primeiro número inteiro: "))
-ex1_num2 = int(input("Digite o segundo número inteiro: "))
-resultado_soma = ex1_num1 + ex1_num2
+# ex1_num1 = int(input("Digite o primeiro número inteiro: "))
+# ex1_num2 = int(input("Digite o segundo número inteiro: "))
+# resultado_soma = ex1_num1 + ex1_num2
 
-print("A soma é:", resultado_soma)
+# print("A soma é:", resultado_soma)
 
-## 02 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# ## 02 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-ex2_div_1 = int(input("Digite um número: "))
-ex2_dividendo = 5
+# ex2_div_1 = int(input("Digite um número: "))
+# ex2_dividendo = 5
 
-resultado_da_divisao = ex2_div_1 // ex2_dividendo
+# resultado_da_divisao = ex2_div_1 // ex2_dividendo
 
-print(resultado_da_divisao)
+# print(resultado_da_divisao)
 
-## 03 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# ## 03 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 
-ex3_num1 = int(input("Digite o primeiro número inteiro: "))
-ex3_num2 = int(input("Digite o segundo número inteiro: "))
-resultado_multiplicacao = ex3_num1 * ex3_num2
+# ex3_num1 = int(input("Digite o primeiro número inteiro: "))
+# ex3_num2 = int(input("Digite o segundo número inteiro: "))
+# resultado_multiplicacao = ex3_num1 * ex3_num2
 
-print("O resultado é:", resultado_multiplicacao)
+# print("O resultado é:", resultado_multiplicacao)
 
-## 04 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# ## 04 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-ex4_num1 = int(input("Digite o primeiro número: "))
-ex4_num2 = int(input("Digite o segundo número: "))
+# ex4_num1 = int(input("Digite o primeiro número: "))
+# ex4_num2 = int(input("Digite o segundo número: "))
 
-divisao = ex4_num1 // ex4_num2
+# divisao = ex4_num1 // ex4_num2
 
-print(divisao)
+# print(divisao)
 
-## 05 -Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# ## 05 -Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-ex5_base = int(input("digite um número: "))
-ex5_expoente = 2
+# ex5_base = int(input("digite um número: "))
+# ex5_expoente = 2
 
-potenciacao = ex5_base ** ex5_expoente
+# potenciacao = ex5_base ** ex5_expoente
 
-print(potenciacao)
+# print(potenciacao)
 
-##Números de Ponto Flutuante (float)
-## 06 - Escreva um programa que receba dois números flutuantes e realize sua adição.
+# ##Números de Ponto Flutuante (float)
+# ## 06 - Escreva um programa que receba dois números flutuantes e realize sua adição.
 
-flo_1 = float(input("Digite o primeiro numero: "))
-flo_2 = float(input("Digite o primeiro numero: "))
+# flo_1 = float(input("Digite o primeiro numero: "))
+# flo_2 = float(input("Digite o primeiro numero: "))
 
-soma = flo_1 + flo_2
+# soma = flo_1 + flo_2
 
-print(soma)
+# print(soma)
 
-## 07 - Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# ## 07 - Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
 
 # med_1 = float(input("Digite o primeiro número: "))
 # med_2 = float(input("Digite o segundo número: "))
@@ -64,6 +64,13 @@ print(soma)
 # print(calculo_media)
 
 ## 08 - Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+pot_1 = float(input("Digite o número da base: "))
+exp_2 = float(input("Digite o número do expoente: "))
+
+calculo_potencia = (pot_1**exp_2)
+
+print(calculo_potencia)
 
 #atualização do git
 
