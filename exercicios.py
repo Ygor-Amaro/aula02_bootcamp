07b33ed914eaa68997b4f52778008b9c139633f1
Ygor Amaro <ykba09@gmail.com>
2025-04-15 23:19:15 -0300

    exercicio 12 ok

--- exercicios.py
+++ exercicios.py
@@ -91,12 +91,18 @@
 ## #Strings (str)
 ## 11 - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
-str_1 = str(input("Digete uma frase aqui: "))
-print_str_1 = str_1.upper()
+# str_1 = str(input("Digite uma frase aqui: "))
+# print_str_1 = str_1.upper()
 
-print(print_str_1)
+# print(print_str_1)
 
 ## 12 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+str_2 = str(input("Digite o texto aqui: "))
+print_str_2 = str_2.lower()
+
+print(print_str_2)
+
 ## 13 - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 ## 14 - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 ## 15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
