db5f70dd51ecab1ebd34516719a3d7d11cd3e359
Ygor Amaro <ykba09@gmail.com>
2025-04-15 23:37:48 -0300

    exercicio 13 ok

--- exercicios.py
+++ exercicios.py
@@ -98,12 +98,18 @@
 
 ## 12 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-str_2 = str(input("Digite o texto aqui: "))
-print_str_2 = str_2.lower()
+# str_2 = str(input("Digite o texto aqui: "))
+# print_str_2 = str_2.lower()
 
-print(print_str_2)
+# print(print_str_2)
 
 ## 13 - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+str_3 = str(input("Digite o texto aqui: "))
+print_str_3 = str_3.strip()
+
+print(print_str_3)
+
 ## 14 - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 ## 15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
