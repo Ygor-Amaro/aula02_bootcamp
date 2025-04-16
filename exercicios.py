dc5be5ab93c76ee306e7eb1e4a30f82066dcc56d
Ygor Amaro <ykba09@gmail.com>
2025-04-15 23:17:34 -0300

    exercicio 11 ok

--- exercicios.py
+++ exercicios.py
@@ -81,15 +81,21 @@
 
 # print(f"{celsus}Cº é igual a {calculo_far}Fº")
 
-## 10 - Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# ## 10 - Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
-raio = float(input("digite o valor do raio: "))
-area = 3.14159 * raio ** 2
+# raio = float(input("digite o valor do raio: "))
+# area = 3.14159 * raio ** 2
 
-print (f"o valor da area do circulo é {round(area)}")
+# print (f"o valor da area do circulo é {round(area)}")
 
 ## #Strings (str)
 ## 11 - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+str_1 = str(input("Digete uma frase aqui: "))
+print_str_1 = str_1.upper()
+
+print(print_str_1)
+
 ## 12 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 ## 13 - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 ## 14 - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
