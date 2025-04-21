cc3cc3bc8dd45eabf658686c043bd7a8226d85f3
Ygor Amaro <ykba09@gmail.com>
2025-04-20 23:00:12 -0300

    exercicios finalizados e testados

--- exercicios.py
+++ exercicios.py
@@ -153,10 +153,16 @@
 
 ## 19 - Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
 
-num1 = 5
-num2 = 5
+# num1 = 5
+# num2 = 5
 
-resultado_igualdade = (num1 == num2)
-print("Resultado da igualdade:", resultado_igualdade)
+# resultado_igualdade = (num1 == num2)
+# print("Resultado da igualdade:", resultado_igualdade)
 
 ## 20 -Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+num1 = 4
+num2 = 5
+
+resultado_diferenca = (num1 != num2)
+print("Resultado da diferença:", resultado_diferenca)
