ba9897b78716d26f120a6c2423531bdf3493da31
Ygor Amaro <ykba09@gmail.com>
2025-04-21 01:21:06 -0300

    exercicio 22 ok

--- desafio.py
+++ desafio.py
@@ -4,21 +4,31 @@ Aqui estão cinco exercícios que envolvem TypeError, verificação de tipo (typ
 Esses exercícios aumentam progressivamente em dificuldade e abordam situações práticas onde você pode aplicar esses conceitos.
 """
 
-# Exercício 21: Conversor de Temperatura
-# Escreva um programa que converta a temperatura de Celsius para Fahrenheit. 
-# O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, 
-# garantir que a entrada seja numérica, tratando qualquer ValueError. Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
+# # Exercício 21: Conversor de Temperatura
+# # Escreva um programa que converta a temperatura de Celsius para Fahrenheit. 
+# # O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, 
+# # garantir que a entrada seja numérica, tratando qualquer ValueError. Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
 
-try:
-    celsius = float(input("Digite a temperatura em Celsius: "))
-    fahrenheit = (celsius * 1.8) + 32
-    print(f"{celsius}°C é igual a {fahrenheit}°F.")
-except ValueError:
-    print("Por favor, digite um número válido para a temperatura.")
+# try:
+#     celsius = float(input("Digite a temperatura em Celsius: "))
+#     fahrenheit = (celsius * 1.8) + 32
+#     print(f"{celsius}°C é igual a {fahrenheit}°F.")
+# except ValueError:
+#     print("Por favor, digite um número válido para a temperatura.")
 
-# Exercício 22: Verificador de Palíndromo
-# Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). 
-# Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
+## Exercício 22: Verificador de Palíndromo
+## Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). 
+## Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
+
+palavra = input("Digite uma palavra: ").lower()
+
+# Remove espaços e pontuações, se necessário
+palavra = ''.join(filter(str.isalnum, palavra))
+
+if palavra == palavra[::-1]:
+    print(f'A palavra "{palavra}" é um palíndromo.')
+else:
+    print(f'A palavra "{palavra}" não é um palíndromo.')
 
 # Exercício 23: Calculadora Simples
 # Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. 
